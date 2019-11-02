f5f59356c5271670895455b2151839ea2414a081
Kjell Nilsen <kjell@familiennilsen.net>
2019-11-02 15:44:00 +0100

    Initial commit

---
+++ KFond-Discovery-Server/src/test/java/net/familiennilsen/KFondDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.familiennilsen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KFondDiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
