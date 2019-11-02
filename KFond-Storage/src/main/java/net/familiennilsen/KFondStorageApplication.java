cd13699ff4fdcbe7efa7d30d7f9685eccf6ae6c9
Kjell Nilsen <kjell@familiennilsen.net>
2019-11-02 16:19:20 +0100

    c

---
+++ KFond-Storage/src/main/java/net/familiennilsen/KFondStorageApplication.java
@@ -0,0 +1,13 @@
+package net.familiennilsen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KFondStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KFondStorageApplication.class, args);
+	}
+
+}
---
+++ KFond-Storage/src/test/java/net/familiennilsen/KFondStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package net.familiennilsen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KFondStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
